9b731a35d117b29ec54dc90f17e6b40eb2d90749
yanding.li <David.Jackson.Lyd@gmail.com>
2021-06-12 22:19:48 +0800

    Harmony Hello World

---
+++ HMtest/entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,32 @@
+export default {
+    data: {
+        title: "aaa",
+        date: new Date()
+    },
+
+    onInit() {
+        this.title = this.$t('strings.world');
+        let date = new Date();
+        this.date = dateFormat("YYYY-mm-dd HH:MM", date);
+
+        function dateFormat(fmt, date) {
+            let ret;
+            const opt = {
+                "Y+": date.getFullYear().toString(),        // 年
+                "m+": (date.getMonth() + 1).toString(),     // 月
+                "d+": date.getDate().toString(),            // 日
+                "H+": date.getHours().toString(),           // 时
+                "M+": date.getMinutes().toString(),         // 分
+                "S+": date.getSeconds().toString()          // 秒
+            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+            };
+            for (let k in opt) {
+                ret = new RegExp("(" + k + ")").exec(fmt);
+                if (ret) {
+                    fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+                };
+            };
+            return fmt;
+        }
+    }
+}
